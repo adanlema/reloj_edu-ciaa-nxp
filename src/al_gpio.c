48da838dcc7ce5aa42e37ad525ca915ab2ba5930
adanlema <adanlema@hotmail.com>
2023-04-25 17:39:11 -0300

    Incorporacion de la carpeta muju y de al_gpio

---
+++ src/al_gpio.c
@@ -0,0 +1,145 @@
+/* Copyright 2022, Adan Lema <adanlema@hotmail.com>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of the copyright holder nor the names of its
+ *    contributors may be used to endorse or promote products derived from this
+ *    software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+/** \brief HAL_GPIO
+ ** \brief Implementacion de las funciones de una HAL_GPIO
+ ** @{ */
+/*==================[inclusions]=============================================*/
+#include "chip.h"
+#include <stdbool.h>
+#include "al_gpio.h"
+/*==================[macros and definitions]=================================*/
+#define CANTIDAD 10
+
+struct DigitalInput {
+    uint8_t port;
+    uint8_t pin;
+    bool    inverted;
+    bool    last_state;
+    bool    ocupado;
+};
+struct DigitalOutput {
+    uint8_t port;
+    uint8_t pin;
+    bool    ocupado;
+};
+
+/*==================[internal data declaration]==============================*/
+static struct DigitalInput  Input[CANTIDAD]  = {0};
+static struct DigitalOutput Output[CANTIDAD] = {0};
+/*==================[internal functions declaration]=========================*/
+
+/*==================[internal data definition]===============================*/
+
+/*==================[external data definition]===============================*/
+
+/*==================[internal functions definition]==========================*/
+
+/*==================[external functions definition]==========================*/
+
+/* Funciones para las Entradas */
+DigitalInput_pt DigitalInput_Create(uint8_t port, uint8_t pin, bool inverted) {
+    uint8_t posicion = 0;
+    for (int i = 0; (i < CANTIDAD) & (posicion == 0); i++) {
+        if (Input[i].ocupado == false)
+            posicion = i;
+    }
+    DigitalInput_pt AL = &Input[posicion];
+    AL->port           = port;
+    AL->pin            = pin;
+    AL->inverted       = inverted;
+    AL->ocupado        = true;
+    Chip_GPIO_SetPinDIR(LPC_GPIO_PORT, port, pin, false);
+    return AL;
+}
+bool DigitalInput_GetState(DigitalInput_pt AL) {
+    if (AL->inverted == 0)
+        return Chip_GPIO_ReadPortBit(LPC_GPIO_PORT, AL->port, AL->pin);
+    else
+        return !(Chip_GPIO_ReadPortBit(LPC_GPIO_PORT, AL->port, AL->pin));
+}
+bool DigitalInput_HasChange(DigitalInput_pt AL) {
+    bool current_state = DigitalInput_GetState(AL);
+    bool resultado;
+    if (current_state != AL->last_state)
+        resultado = true;
+    else
+        resultado = false;
+    AL->last_state = current_state;
+    return resultado;
+}
+bool DigitalInput_HasActivate(DigitalInput_pt AL) {
+    bool current_state = DigitalInput_GetState(AL);
+    bool resultado;
+    if ((0 == AL->last_state) & (1 == current_state))
+        resultado = true;
+    else
+        resultado = false;
+    AL->last_state = current_state;
+    return resultado;
+}
+bool DigitalInput_HasDesactivate(DigitalInput_pt AL) {
+    bool current_state = DigitalInput_GetState(AL);
+    bool resultado;
+    if ((1 == AL->last_state) & (0 == current_state))
+        resultado = true;
+    else
+        resultado = false;
+    AL->last_state = current_state;
+    return resultado;
+}
+
+/* Funciones para las Salidas */
+DigitalOutput_pt DigitalOutput_Create(uint8_t port, uint8_t pin) {
+    uint8_t posicion = 0;
+    for (int i = 0; (i < CANTIDAD) & (posicion == 0); i++) {
+        if (Output[i].ocupado == false)
+            posicion = i;
+    }
+    DigitalOutput_pt AL = &Output[posicion];
+    AL->port            = port;
+    AL->pin             = pin;
+    AL->ocupado         = true;
+    Chip_GPIO_SetPinState(LPC_GPIO_PORT, port, pin, false);
+    Chip_GPIO_SetPinDIR(LPC_GPIO_PORT, port, pin, true);
+    return AL;
+}
+void DigitalOutput_Activate(DigitalOutput_pt AL) {
+    Chip_GPIO_SetPinState(LPC_GPIO_PORT, AL->port, AL->pin, true);
+}
+void DigitalOutput_Desactivate(DigitalOutput_pt AL) {
+    Chip_GPIO_SetPinState(LPC_GPIO_PORT, AL->port, AL->pin, false);
+}
+void DigitalOutput_Toggle(DigitalOutput_pt AL) {
+    Chip_GPIO_SetPinToggle(LPC_GPIO_PORT, AL->port, AL->pin);
+}
+
+/**  doxygen end group definition */
+/*==================[end of file]============================================*/
---
+++ src/al_gpio.h
@@ -0,0 +1,120 @@
+/* Copyright 2022, Adan Lema <adanlema@hotmail.com>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of the copyright holder nor the names of its
+ *    contributors may be used to endorse or promote products derived from this
+ *    software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+/** \brief HAL_GPIO
+ ** \brief Implementacion de las funciones de una HAL_GPIO
+ ** @{ */
+
+#ifndef AL_GPIO_H
+#define AL_GPIO_H
+
+/*==================[inclusions]=============================================*/
+typedef struct DigitalInput *  DigitalInput_pt;
+typedef struct DigitalOutput * DigitalOutput_pt;
+/*==================[macros]=================================================*/
+
+/*==================[typedef]================================================*/
+
+/*==================[external data declaration]==============================*/
+
+/*==================[external functions declaration]=========================*/
+/**
+ * @brief Digital Input Create
+ * Se encarga de configurar como entrada digital a un pin proporcionado.
+ * @param port Puerto
+ * @param pin Pin
+ * @param inverted Si trabajo con Pull-Down(false) o Pull-Up(true)
+ * @return DigitalInput_pt 
+ */
+DigitalInput_pt  DigitalInput_Create(uint8_t port, uint8_t pin, bool inverted);
+/**
+ * @brief Digital Input Get State
+ * Devuelve el estado de la entrada.
+ * @param AL Puntero al struct de la entrada.
+ * @return true Si se encuentra activo.
+ * @return false Si no esta activo.
+ */
+bool             DigitalInput_GetState(DigitalInput_pt AL);
+/**
+ * @brief Digital Input Has Change
+ * Evalua si la entrada a cambiado.
+ * @param AL Puntero al struct de la entrada.
+ * @return true Si cambio.
+ * @return false Si no cambio.
+ */
+bool             DigitalInput_HasChange(DigitalInput_pt AL);
+/**
+ * @brief Digital Input Has Activate
+ * Evalua si la entrada a sido activada.
+ * @param AL Puntero al struct de la entrada.
+ * @return true Si se activo.
+ * @return false Si no se activo.
+ */
+bool             DigitalInput_HasActivate(DigitalInput_pt AL);
+/**
+ * @brief Digital Input Has Desactivate
+ * Evalua si la entrada a sido desactivada.
+ * @param AL Puntero al struct de la entrada.
+ * @return true Si se desactivo.
+ * @return false Si sigue activa.
+ */
+bool             DigitalInput_HasDesactivate(DigitalInput_pt AL);
+/**
+ * @brief Digital Output Create
+ * Crea una salida digital, en la cual debemos proporcionar el pin y puerto.
+ * Retorna el puntero de la esctructura creada.
+ * @param port Puerto
+ * @param pin Pin
+ * @return DigitalOutput_pt 
+ */
+DigitalOutput_pt DigitalOutput_Create(uint8_t port, uint8_t pin);
+/**
+ * @brief Digital Output Activate
+ * Activa la salida solicitada.
+ * @param AL Puntero a la salida digital.
+ */
+void             DigitalOutput_Activate(DigitalOutput_pt AL);
+/**
+ * @brief Digital Output Desactivate
+ * Desactiva la salida solicitada.
+ * @param AL Puntero a la salida digital.
+ */
+void             DigitalOutput_Desactivate(DigitalOutput_pt AL);
+/**
+ * @brief Digital Output Toggle
+ * Cambia el estado de la salida proporcionada.
+ * @param AL Puntero a la salida digital.
+ */
+void             DigitalOutput_Toggle(DigitalOutput_pt AL);
+/** @ doxygen end group definition */
+/** @ doxygen end group definition */
+/** @ doxygen end group definition */
+/*==================[end of file]============================================*/
+#endif
