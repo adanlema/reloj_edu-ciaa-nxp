fe4a433126fc288af982fc18118e042572bf9aab
adanlema <adanlema@hotmail.com>
2023-05-24 11:01:16 -0300

    agregado del display, recien comenzado...

---
+++ src/al_display7seg.h
@@ -0,0 +1,22 @@
+/* Copyright 2023, Adan Lema <adanlema@hotmail.com> */
+
+#ifndef AL_DISPLAY7_SEG_H
+#define AL_DISPLAY7_SEG_H
+
+/*==================[inclusions]=============================================*/
+
+/*==================[macros]=================================================*/
+
+/*==================[typedef]================================================*/
+typedef struct display_s * display_pt;
+typedef void (*funcion_pt)(display_pt);
+
+/*==================[external data declaration]==============================*/
+
+/*==================[external functions declaration]=========================*/
+
+/** @ doxygen end group definition */
+/** @ doxygen end group definition */
+/** @ doxygen end group definition */
+/*==================[end of file]============================================*/
+#endif
