efe03c318f8c81ca21f9ddfa016ecfc83f497191
adanlema <adanlema@hotmail.com>
2023-06-22 16:54:07 -0300

    algunas pequeñas correcciones. El reloj funciona como corresponde.

--- src/main.c
+++ src/main.c
@@ -14,6 +14,10 @@
 #define INT_POR_SEG   1000
 #define TICKS_POR_SEG 100
 #define ESPERA_SEG    3000
+
+#define CONT_TIME     0
+#define CONT_ALRM     1
+#define MAX_CONT      2
 /*==================[internal data declaration]==============================*/
 
 /*==================[internal functions declaration]=========================*/
@@ -112,22 +116,23 @@ int main(void) {
         }
 
         if (DigitalInput_HasActivate(board_educia->f1)) {
-            contador_pulsos[0]++;
+            contador_pulsos[CONT_TIME] = 1;
         }
-        if (contador_pulsos[0] > 3000) {
-            contador_pulsos[0] = 0;
-            contador_pulsos[3]++;
+        if (contador_pulsos[CONT_TIME] > (INT_POR_SEG * 3)) {
+            contador_pulsos[CONT_TIME] = 0;
+            contador_pulsos[MAX_CONT]  = 1;
+            DisplayParpadeoPuntos(board_educia->display, (uint8_t[]){0, 0, 0, 0}, 0);
             CambiarModo(AJUSTAR_MINUTOS_ACTUAL);
             ClockGetTime(reloj, hora_auxiliar, sizeof(hora_auxiliar));
             DisplayWriteBCD(board_educia->display, hora_auxiliar, sizeof(hora_auxiliar));
         }
 
         if (DigitalInput_HasActivate(board_educia->f2)) {
-            contador_pulsos[1]++;
+            contador_pulsos[CONT_ALRM] = 1;
         }
-        if (contador_pulsos[1] > 3000) {
-            contador_pulsos[1] = 0;
-            contador_pulsos[3]++;
+        if (contador_pulsos[CONT_ALRM] > (INT_POR_SEG * 3)) {
+            contador_pulsos[CONT_ALRM] = 0;
+            contador_pulsos[MAX_CONT]  = 1;
             DisplayPuntoAlarma(board_educia->display, false);
             CambiarModo(AJUSTAR_MINUTOS_ALARMA);
             ClockGetAlarma(reloj, hora_auxiliar, sizeof(hora_auxiliar));
@@ -160,8 +165,8 @@ int main(void) {
             }
         }
 
-        if (contador_pulsos[3] > (INT_POR_SEG * 30)) {
-            contador_pulsos[3] = 0;
+        if (contador_pulsos[MAX_CONT] > (INT_POR_SEG * 30)) {
+            contador_pulsos[MAX_CONT] = 0;
             CambiarModo(MOSTRANDO_HORA);
         }
     }
@@ -174,16 +179,16 @@ void SysTick_Handler(void) {
     DisplayRefresh(board_educia->display);
     ClockTick(reloj);
     if ((DigitalInput_GetState(board_educia->f1)) && (contador_pulsos[0] > 0)) {
-        contador_pulsos[0]++;
+        contador_pulsos[CONT_TIME]++;
     }
     if ((DigitalInput_GetState(board_educia->f2)) && (contador_pulsos[1] > 0)) {
-        contador_pulsos[1]++;
+        contador_pulsos[CONT_ALRM]++;
     }
     if (!((DigitalInput_GetState(board_educia->f2))) &&
         !((DigitalInput_GetState(board_educia->f1))) &&
         !((DigitalInput_GetState(board_educia->aceptar))) &&
         !((DigitalInput_GetState(board_educia->rechazar))) && (contador_pulsos[3] > 0)) {
-        contador_pulsos[3]++;
+        contador_pulsos[CONT_ALRM]++;
     }
 
     if (modo <= MOSTRANDO_HORA) {
